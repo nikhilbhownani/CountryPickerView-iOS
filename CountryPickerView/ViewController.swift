87bcd8f8a6eb2903c14d4123887b47f57e6835fb
Nikhil <nikhil@fueled.co>
2019-05-10 00:08:14 +0530

    add functionality for showing all countries, some countries. add functionality to show as modal / popup

--- CountryPickerView/ViewController.swift
+++ CountryPickerView/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pickerView: CountryPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pickerView.setupView(nibName: "CountryPickerImageNameCell")
+//        pickerView.showCountries(codes: ["DK", "SE", "NO", "DE","US"])
+        pickerView.showAllCountries()
+        pickerView.isModal = true
+        pickerView.onDidSelect = { [weak self](country) in
+            print(country)
+            UIView.animate(withDuration: 0.2, animations: {
+                self?.pickerView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+            }, completion: { (competion) in
+                self?.pickerView.isHidden = true
+                self?.pickerView.transform = CGAffineTransform.identity
+            })
+            
+        }
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        showPickerView()
+    }
+    
+    func showPickerView() {
+        pickerView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        pickerView.isHidden = false
+        UIView.animate(withDuration: 0.2, animations: {
+            self.pickerView.transform = CGAffineTransform.identity
+        }) { (completed) in
+            
+        }
+    }
+    
 }
 
